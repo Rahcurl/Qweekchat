567e1f02a95dbe88367a8f98613061cd037eaea8
Rahcurl <rsharma194@rku.ac.in>
2026-02-01 00:25:47 +0545

    schema creating for atuh

---
+++ backend/src/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true
+    }
+    ,
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    },
+    bio:{
+        type:String,
+        default:""
+    },
+    profilePic:{
+        type:String,
+        default:""
+    },
+    nativeLanguage:{
+        type:String,
+        required:true   
+    },
+    learningLanguages:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        default:""
+    },
+    isOnboard:{
+        type:Boolean,
+        default:false
+    },
+    friends:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref :"User"
+        }
+    ]
+},{timestamps:true});
